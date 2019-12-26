e27dd2914c4b78302ee5a08cfbe3a1c0f598604e
Giovanni <giovanniantonaccio@gmail.com>
2019-12-26 10:20:55 -0400

    Add reset password functionality

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const subDays = require('date-fns/subDays');
+const isBefore = require('date-fns/isBefore');
 const crypto = require('crypto');
 
 const User = use('App/Models/User');
@@ -8,7 +10,8 @@ const Mail = use('Mail');
 class ForgotPasswordController {
   async store({ request, response }) {
     try {
-      const email = request.input('email');
+      const { email, redirect_url } = request.only(['email', 'redirect_url']);
+
       const user = await User.findByOrFail('email', email);
 
       user.token = crypto.randomBytes(10).toString('hex');
@@ -21,7 +24,7 @@ class ForgotPasswordController {
         {
           email: user.email,
           token: user.token,
-          link: `${request.input('redirect_url')}?token=${user.token}`,
+          link: `${redirect_url}?token=${user.token}`,
         },
         message => {
           message.subject('Recuperação de Senha');
@@ -32,6 +35,35 @@ class ForgotPasswordController {
     } catch (err) {
       return response.status(err.status).send({
         error: { message: 'Algo não deu certo, esse e-mail existe?' },
+      });
+    }
+  }
+
+  async update({ request, response }) {
+    try {
+      const { token, password } = request.all();
+
+      const user = await User.findByOrFail('token', token);
+
+      const tokenExpired = isBefore(
+        user.token_created_at,
+        subDays(new Date(), 2)
+      );
+
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: { message: 'O token de recuperação está expirado' },
+        });
+      }
+
+      user.token = null;
+      user.token_created_at = null;
+      user.password = password;
+
+      await user.save();
+    } catch (err) {
+      return response.status(err.status).send({
+        error: { message: 'Algo deu errado ao resetar sua senha' },
       });
     }
   }
